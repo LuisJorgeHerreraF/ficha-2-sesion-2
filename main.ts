b3237ff109f2c6bec678bbbb34095e93afe4eb3e
LuisJorgeHerreraF <95259402+LuisJorgeHerreraF@users.noreply.github.com>
2024-09-01 18:04:19 -0500

    Actualizar pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,11 +1,20 @@
+/**
+ * Si T >34ºC no se desarrollan
+ * 
+ * Si T<26ºC no se desarrollan
+ */
 basic.forever(function () {
     basic.showNumber(input.temperature())
-    while (input.temperature() > 25) {
+    while (input.temperature() > 34) {
         basic.showNumber(input.temperature())
-        basic.showIcon(IconNames.Happy)
+        basic.showString("T. Alta")
     }
-    while (input.temperature() < 23) {
+    while (input.temperature() < 26) {
         basic.showNumber(input.temperature())
-        basic.showIcon(IconNames.Sad)
+        basic.showString("T. Baja")
+    }
+    while (input.temperature() >= 26 && input.temperature() <= 34) {
+        basic.showNumber(input.temperature())
+        basic.showString("T. Normal")
     }
 })
